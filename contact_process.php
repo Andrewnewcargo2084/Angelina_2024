56a3825461e12b3041b6507a2379a535dd1f99e9
Andrewnewcargo2084 <andrewcomp014@gmail.com>
2024-11-11 21:16:05 +0300

    Add files via upload

--- contact_process.php
+++ contact_process.php
@@ -1,6 +1,6 @@
 <?php
 
-    $to = "rockybd1995@gmail.com";
+    $to = "makeeva.angelna05@gmail.com";
     $from = $_REQUEST['email'];
     $name = $_REQUEST['name'];
     $subject = $_REQUEST['subject'];
